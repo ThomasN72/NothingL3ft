dff8171e7f80256a08bff6a992c784afa3297ea5
sandond77 <sandu@ucdavis.edu>
2017-12-20 16:06:18 -0800

    Added autocomplete function for search bar

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -193,3 +193,29 @@ function restaurantFinder(location,pos){
 }
 
 
+var placeSearch, autocomplete, geocoder;
+
+function initAutocomplete() {
+  geocoder = new google.maps.Geocoder();
+  autocomplete = new google.maps.places.Autocomplete(
+      (document.getElementById('search'))/*,
+      {types: ['(cities)']}*/);
+
+  autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function codeAddress(address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        alert(results[0].geometry.location);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+function fillInAddress() {
+  var place = autocomplete.getPlace();
+  alert(place.place_id);
+  //   codeAddress(document.getElementById('autocomplete').value);
+}
